131f17bf790fc8a70d533ebe25ac73921f216e7d
Max Ghenis <mghenis@gmail.com>
2023-07-06 12:31:46 -0400

    make format

--- policyengine_il/variables/gov/low_income_municipal_tax_reduction/low_income_municipal_tax_reduction_income.py
+++ policyengine_il/variables/gov/low_income_municipal_tax_reduction/low_income_municipal_tax_reduction_income.py
@@ -3,7 +3,9 @@ from policyengine_il.model_api import *
 
 class low_income_municipal_tax_reduction_income(Variable):
     label = "Low-income municipal tax reduction income"
-    documentation = "Municipalities base their municipal tax reduction rate on this income"
+    documentation = (
+        "Municipalities base their municipal tax reduction rate on this income"
+    )
     entity = Household
     definition_period = YEAR
     value_type = float
