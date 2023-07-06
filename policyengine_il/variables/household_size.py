9a55c49a394b3d96e49a61306e664360827d1147
Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>
2023-07-06 16:56:26 +0100

    Add changes

---
+++ policyengine_il/variables/household_size.py
@@ -0,0 +1,10 @@
+from policyengine_il.model_api import *
+
+
+class household_size(Variable):
+    label = "Household size"
+    documentation = "Size of a household"
+    entity = Household
+    definition_period = YEAR
+    value_type = int
+    unit = "person"
